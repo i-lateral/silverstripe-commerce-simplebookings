3ccb51241cc5f75fc0aa978a31d66daa847cdae3
Morven <morven@i-lateral.com>
2017-02-24 15:24:57 +0000

    Add ability to filter bookings from the modeladmin by start and end dates

--- code/admin/BookingAdmin.php
+++ code/admin/BookingAdmin.php
@@ -17,4 +17,65 @@ class BookingAdmin extends ModelAdmin
     );
 
     public $showImportForm = array('Booking');
+
+    /**
+	 * @return Form
+	 */
+	public function SearchForm() {
+        $form = parent::SearchForm();
+        $fields = $form->Fields();
+        $query = $this->getRequest()->getVar("q");
+
+        $fields->replaceField(
+            "q[Start]",
+            $start_field = DateField::create("q[StartDate]", _t("SimpleBookings.StartDate", "Start Date"))
+                ->setConfig('dateformat', 'yyyy-MM-dd')
+                ->setConfig('showcalendar', true)
+        );
+
+        $fields->replaceField(
+            "q[End]",
+            $end_field = DateField::create("q[EndDate]", _t("SimpleBookings.EndDate", "End Date"))
+                ->setConfig('dateformat', 'yyyy-MM-dd')
+                ->setConfig('showcalendar', true)
+        );
+
+        if (is_array($query) && array_key_exists("StartDate", $query)) {
+            $start_field->setValue($query["StartDate"]);
+        }
+
+        if (is_array($query) && array_key_exists("EndDate", $query)) {
+            $end_field->setValue($query["EndDate"]);
+        }
+
+		$this->extend('updateSearchForm', $form);
+
+		return $form;
+	}
+
+    public function getList()
+    {
+        $list = parent::getList();
+
+        $filter = array();
+        
+        // Perform complex filtering
+        if ($this->modelClass == 'Booking') {
+            $query = $this->getRequest()->getVar("q");
+
+            // If a start date and end date are set, filter all dates
+            if (is_array($query) && array_key_exists("StartDate", $query) && array_key_exists("EndDate", $query)) {
+                $filter["Start:GreaterThanOrEqual"] = $query["StartDate"];
+                $filter["End:LessThanOrEqual"] = $query["EndDate"];
+            }
+        }
+
+        if (count($filter)) {
+            $list = $list->filter($filter);
+        }
+        
+        $this->extend('updateList', $list);
+
+        return $list;
+    }
 }
