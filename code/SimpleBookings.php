073920beff97397017eb97ae968fdf116fb8310d
Morven Lewis-Everley <morven@i-lateral.com>
2022-05-31 15:08:38 +0100

    Add simple cache to places check to reduce DB overhead on complex queries

--- code/SimpleBookings.php
+++ code/SimpleBookings.php
@@ -30,22 +30,13 @@ class SimpleBookings extends ViewableData
     private static $allow_delivery = false;
 
     /**
-     * Provy for createDateRangeArray() for backwards compatibility
+     * Some of the codebase results in querying getTotalBookedSpaces
+     * repeatedly. Try to overcome performance bottlenecks
+     * by caching product ID's VS a number of spaces
      *
-     * @param string $date_from The starting date
-     * @param string $date_to   The end date
-     * @param int    $interval  Time period (seconds) to use to make the array
-     *
-     * @return array
+     * @var array
      */
-    public static function create_date_range_array($date_from, $date_to, $interval)
-    {
-        Deprecation::notice(
-            "0.5.0",
-            "Please use SimpleBookings::createDateRangeArray()"
-        );
-        return self::createDateRangeArray($date_from, $date_to, $interval);
-    }
+    private static $cached_spaces = [];
 
     /**
      * Takes two dates formatted as YYYY-MM-DD and creates an inclusive
@@ -77,24 +68,6 @@ class SimpleBookings extends ViewableData
     }
 
     /**
-     * Proxy for getTotalSpaces until it is depreciated
-     * 
-     * @param string $date_from The starting date
-     * @param string $date_to   The end date
-     * @param int    $ID        The ID of the product we are trying to book
-     * 
-     * @return int
-     */
-    public static function get_total_booked_spaces($date_from, $date_to, $ID)
-    {
-        Deprecation::notice(
-            "0.5.0",
-            "Please use SimpleBookings::getTotalBookedSpaces()"
-        );
-        return self::getTotalBookedSpaces($date_from, $date_to, $ID);
-    }
-
-    /**
      * Find the total spaces already booked between the two provided dates.
      *
      * @param string $date_from The starting date
@@ -105,12 +78,18 @@ class SimpleBookings extends ViewableData
      */
     public static function getTotalBookedSpaces($date_from, $date_to, $ID)
     {
+        // Attempt to return spaces from local cache if
+        // possible
+        $cache = self::$cached_spaces;
+
+        if (in_array($ID, array_keys($cache))) {
+            return $cache[$ID];
+        }
+
         $db = DB::get_conn();
 
         // First get a list of days between the start and end date
-        $total_places = 0;
-        $product = BookableProduct::get()->byID($ID);
-        
+        $total_places = 0;        
         $format = "%Y-%m-%d";
 
         $start_field = $db->formattedDatetimeClause(
@@ -130,72 +109,73 @@ class SimpleBookings extends ViewableData
             "ProductID" => $ID
         ];
 
-        if ($product) {
-            // Get all bookings with a start date within
-            // the date range
-            $bookings = BookingResource::get()->where(
-                $date_filter
-            );
-            
-            // Now get all products inside these bookings that
-            // match our date range and tally the results
-            foreach ($bookings as $match_product) {
-                $start_stamp = strtotime($date_from);
-                $end_stamp = strtotime($date_to);
-                $prod_start_stamp = strtotime($match_product->Start);
-                $prod_end_stamp = strtotime($match_product->End);
+        // Get all bookings with a start date within
+        // the date range
+        $bookings = BookingResource::get()->where(
+            $date_filter
+        );
+        
+        // Now get all products inside these bookings that
+        // match our date range and tally the results
+        foreach ($bookings as $match_product) {
+            $start_stamp = strtotime($date_from);
+            $end_stamp = strtotime($date_to);
+            $prod_start_stamp = strtotime($match_product->Start);
+            $prod_end_stamp = strtotime($match_product->End);
 
-                if ($prod_start_stamp >= $start_stamp && $prod_start_stamp <= $end_stamp 
-                    || $prod_start_stamp <= $start_stamp && $prod_end_stamp >= $end_stamp 
-                    || $prod_end_stamp >= $start_stamp && $prod_end_stamp <= $end_stamp
-                ) {
-                    $total_places += $match_product->BookedQTY;
-                }
+            if ($prod_start_stamp >= $start_stamp && $prod_start_stamp <= $end_stamp 
+                || $prod_start_stamp <= $start_stamp && $prod_end_stamp >= $end_stamp 
+                || $prod_end_stamp >= $start_stamp && $prod_end_stamp <= $end_stamp
+            ) {
+                $total_places += $match_product->BookedQTY;
+            }
+        }
+
+        $date_filter = [
+            $start_field . ' <= ?' =>  $date_to,
+            $start_field . ' >= ?' =>  $date_from,
+            $end_field . ' >= ?' =>  $date_from,
+            $end_field . ' <= ?' =>  $date_to
+        ];
+
+        // Get all bookings with a start date within
+        // the date range
+        $allocations = ResourceAllocation::get()->where($date_filter);
+
+        $all_allocated = false;
+
+        foreach ($allocations as $allocation) {
+            if ($allocation->AllocateAll) {
+                $all_allocated = true;
             }
 
-            $date_filter = [
-                $start_field . ' <= ?' =>  $date_to,
-                $start_field . ' >= ?' =>  $date_from,
-                $end_field . ' >= ?' =>  $date_from,
-                $end_field . ' <= ?' =>  $date_to
-            ];
+            $resources = $allocation->Resources()->Filter('ID', $ID);
+            
+            foreach ($resources as $product) {
+                if ($all_allocated || $product->AllocateAll) {
+                    $total_places += $product->AvailablePlaces;
+                } else {
+                    $start_stamp = strtotime($date_from);
+                    $end_stamp = strtotime($date_to);
+                    $alloc_start_stamp = strtotime($allocation->Start);
+                    $alloc_end_stamp = strtotime($allocation->End);
 
-            // Get all bookings with a start date within
-            // the date range
-            $allocations = ResourceAllocation::get()->where($date_filter);
-
-            $all_allocated = false;
-
-            foreach ($allocations as $allocation) {
-                if ($allocation->AllocateAll) {
-                    $all_allocated = true;
-                }
-
-                $resources = $allocation->Resources()->Filter('ID', $ID);
-                
-                foreach ($resources as $product) {
-                    if ($all_allocated || $product->AllocateAll) {
-                        $total_places += $product->AvailablePlaces;
-                    } else {
-                        $start_stamp = strtotime($date_from);
-                        $end_stamp = strtotime($date_to);
-                        $alloc_start_stamp = strtotime($allocation->Start);
-                        $alloc_end_stamp = strtotime($allocation->End);
-
-                        if ($alloc_start_stamp >= $start_stamp && $alloc_start_stamp <= $end_stamp 
-                            || $alloc_start_stamp <= $start_stamp && $alloc_end_stamp >= $end_stamp 
-                            || $alloc_end_stamp >= $start_stamp && $alloc_end_stamp <= $end_stamp
-                        ) {
-                            if ($product->Increase) {
-                                $total_places -= $product->Quantity;
-                            } else {
-                                $total_places += $product->Quantity;
-                            }
+                    if ($alloc_start_stamp >= $start_stamp && $alloc_start_stamp <= $end_stamp 
+                        || $alloc_start_stamp <= $start_stamp && $alloc_end_stamp >= $end_stamp 
+                        || $alloc_end_stamp >= $start_stamp && $alloc_end_stamp <= $end_stamp
+                    ) {
+                        if ($product->Increase) {
+                            $total_places -= $product->Quantity;
+                        } else {
+                            $total_places += $product->Quantity;
                         }
                     }
                 }
             }
         }
+
+        // Add this result to the local cache
+        self::$cached_spaces[$ID] = $total_places;
 
         return $total_places;
     }
